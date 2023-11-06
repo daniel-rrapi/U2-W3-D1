2d15c11581b3dfac00a7569b5bec2b93d271d58d
Daniel Rrapi <uzo1209@gmail.com>
2023-11-06 15:22:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+class User {
+  constructor(firstName, lastName, age, location) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.location = location;
+  }
+}
