029c5f9ecdec68fbada67aa03a9df889e18261ba
Daniel Rrapi <uzo1209@gmail.com>
2023-11-06 18:02:04 +0100

    second commit

--- script.js
+++ script.js
@@ -6,3 +6,41 @@ class User {
     this.location = location;
   }
 }
+
+//2° esercizio
+class Pet {
+  constructor(petName, ownerName, species, breed) {
+    this.petName = petName;
+    this.ownerName = ownerName;
+    this.species = species;
+    this.breed = breed;
+  }
+}
+
+list = [];
+
+const form = document.getElementById("pet");
+const ul = document.getElementById("newUl");
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+  let petName = document.getElementById("petname").value;
+  let ownerName = document.getElementById("owner").value;
+  let species = document.getElementById("species").value;
+  let breed = document.getElementById("breed").value;
+
+  const newPet = new Pet(petName, ownerName, species, breed);
+  list.push(newPet);
+  let li = document.createElement("li");
+  li.innerText =
+    newPet.petName +
+    " " +
+    newPet.ownerName +
+    " " +
+    newPet.species +
+    " " +
+    newPet.breed;
+  ul.appendChild(li);
+  console.log(list);
+};
+// console.log(list);
